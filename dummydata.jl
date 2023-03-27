643945c8b0ded58ee2461e9e37da68da5bd1a9c4
Frankie Robertson <frankie@robertson.name>
2023-03-27 10:08:19 +0300

    Fix broken slip

--- dummydata.jl
+++ dummydata.jl
@@ -6,8 +6,8 @@ function clumpy_4pl_item_bank(rng, num_clumps, num_questions)
     clump_dist_mat = hcat(
         Normal.(rand(rng, Normal(), num_clumps), 0.1),  # Difficulty
         Normal.(abs_rand(rng, Normal(1.0, 0.2), num_clumps), 0.1),  # Discrimination
-        Normal.(clamp_rand(rng, Normal(0.0, 0.2), num_clumps), 0.02),  # Guess
-        Normal.(clamp_rand(rng, Normal(0.0, 0.2), num_clumps))  # Slip
+        Normal.(clamp_rand(rng, Normal(0.1, 0.2), num_clumps), 0.02),  # Guess
+        Normal.(clamp_rand(rng, Normal(0.1, 0.2), num_clumps), 0.02)  # Slip
     )
     params_clumps = mapslices(Product, clump_dist_mat; dims=[2])[:, 1]
     # TODO: Resample the clumps to create a correlated distribution
